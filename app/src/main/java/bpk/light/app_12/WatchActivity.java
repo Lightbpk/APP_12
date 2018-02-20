aa0af4c29dd85255bf23c700cf40dd8ccf6dec18
Lightbpk <ebKeir-+>
2018-02-20 11:26:44 +0700

    20.02 start2 rebild

--- app/src/main/java/bpk/light/app_12/WatchActivity.java
+++ app/src/main/java/bpk/light/app_12/WatchActivity.java
@@ -1,6 +1,5 @@
 package bpk.light.app_12;
 
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +9,7 @@ public class WatchActivity extends AppCompatActivity{
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch);
     }
 }
 
