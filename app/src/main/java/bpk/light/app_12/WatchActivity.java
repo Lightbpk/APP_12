b1dcbb7551483a49cbdbdc23159638c842327fbc
Lightbpk <ebKeir-+>
2018-02-22 17:32:59 +0700

    ready 1 fragment

--- app/src/main/java/bpk/light/app_12/WatchActivity.java
+++ app/src/main/java/bpk/light/app_12/WatchActivity.java
@@ -5,8 +5,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ImageFormat;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
@@ -15,6 +17,7 @@ import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -32,12 +35,18 @@ public class WatchActivity extends AppCompatActivity implements SurfaceHolder.Ca
     Camera mCamera;
     DrawThread drawThread;
     Bitmap bmp;
+    int dw, dh;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_watch);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        dw = size.x;
+        dh = size.y;
         sw = findViewById(R.id.camSurface);
         drawSW = findViewById(R.id.drawSurface);
         surfaceHolder  = sw.getHolder();
@@ -137,7 +146,7 @@ public class WatchActivity extends AppCompatActivity implements SurfaceHolder.Ca
             Canvas canvas;
             Paint pD,pS;
             Path path;
-            Bitmap bitmap;
+            Bitmap bitmap, half, coled;
             Log.d("LightLog","Drawing start");
             while (running) {
                 canvas = null;
@@ -149,8 +158,18 @@ public class WatchActivity extends AppCompatActivity implements SurfaceHolder.Ca
                         continue;
                     }
                     else Log.d("LightLog","Canvas not Null");
-                    canvas.drawBitmap(getCroppedBitmap(bmp,true),0,0,null);
-                    Log.d("LightLog","Drawing Green");
+                        Matrix mScale = new Matrix();
+                        mScale.setScale((float) 0.2, (float) 0.2);
+                        bitmap = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), mScale, true);
+                        half = getCroppedBitmap(bitmap, true);
+                        canvas.drawBitmap(half, 0, 0, null);
+                        Matrix matrix = new Matrix();
+                        matrix.preScale(1.0f, -1.0f);
+                        matrix.setRotate(-180);
+                        coled = Bitmap.createBitmap(half, 0, 0, half.getWidth(), half.getHeight(), matrix, true);
+                        canvas.drawBitmap(coled, 0, 0, null);
+                        Log.d("LightLog", "Drawing Green");
+
                 } finally {
                     if (canvas != null) {
                         surfaceHolder.unlockCanvasAndPost(canvas);
